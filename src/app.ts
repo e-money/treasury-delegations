b5466b735708209a7240d65d035168b823204e48
Martin Dyring-Andersen <martin@dyring-andersen.dk>
2021-06-14 15:51:49 +0200

    Generate txs

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,11 @@ import { Bech32 } from '@cosmjs/encoding'
 import {
   LcdClient,
   setupDistributionExtension,
-  setupStakingExtension
+  setupStakingExtension,
+  coin,
+  Msg,
+  MsgDelegate,
+  MsgUndelegate
 } from '@cosmjs/launchpad'
 import { BondStatus } from '@cosmjs/launchpad/build/lcdapi/staking.js'
 import { median } from 'mathjs'
@@ -125,7 +129,86 @@ async function createTargets (validators): Promise<Target[]> {
     return left.moniker.localeCompare(right.moniker)
   })
 
+  console.log(`Targets: ${result.length}`)
   return result
+}
+
+async function createMessages (targets: Target[]): Promise<Msg[]> {
+  const result: Msg[] = []
+  const delegations = await client.staking.delegatorDelegations(treasuryAddress)
+
+  // Adjust existing delegations
+  for (const delegation of delegations.result) {
+    const target = targets.find(item => item.operatorAddress === delegation.validator_address)
+    if (!target) {
+      console.log(`Skipping: ${delegation.validator_address}`)
+      continue
+    }
+    const targetDelegation = target ? target.totalDelegation : 0
+    const deltaDelegation = targetDelegation - Number(delegation.balance.amount)
+    if (deltaDelegation > 0) {
+      const msg: MsgDelegate = {
+        type: 'cosmos-sdk/MsgDelegate',
+        value: {
+          delegator_address: treasuryAddress,
+          validator_address: delegation.validator_address,
+          amount: coin(deltaDelegation, 'ungm')
+        }
+      }
+      result.push(msg)
+    } else if (deltaDelegation < 0) {
+      const msg: MsgUndelegate = {
+        type: 'cosmos-sdk/MsgUndelegate',
+        value: {
+          delegator_address: treasuryAddress,
+          validator_address: delegation.validator_address,
+          amount: coin(-deltaDelegation, 'ungm')
+        }
+      }
+      result.push(msg)
+    }
+  }
+
+  // Add new delegations
+  for (const target of targets) {
+    const delegation = delegations.result.find(item => item.validator_address === target.operatorAddress)
+    if (!delegation) {
+      const msg: MsgDelegate = {
+        type: 'cosmos-sdk/MsgDelegate',
+        value: {
+          delegator_address: treasuryAddress,
+          validator_address: target.operatorAddress,
+          amount: coin(target.totalDelegation, 'ungm')
+        }
+      }
+      result.push(msg)
+    }
+  }
+  return result
+}
+
+async function writeTransaction (messages: Msg[], fileName: string) {
+  const gasEstimate = messages.length * 500000
+  const gasPrice = 1
+  const transaction = {
+    type: 'cosmos-sdk/StdTx',
+    value: {
+      msg: messages,
+      fee: {
+        amount: [
+          {
+            denom: 'ungm',
+            amount: (gasEstimate * gasPrice).toFixed(0)
+          }
+        ],
+        gas: gasEstimate.toFixed(0)
+      },
+      signatures: null,
+      memo: 'Treasury Delegations'
+    }
+  }
+  const buffer = JSON.stringify(transaction, null, 2)
+  writeFileSync(fileName, buffer)
 }
 
 function writeCsv (targets: Target[], fileName: string) {
@@ -137,12 +220,12 @@ function writeCsv (targets: Target[], fileName: string) {
   }
   console.dir({ currentTotalDelegations, updatedTotalDelegations })
 
-  let buffer = 'Moniker,Commission,Number of Delegations,Current Self Delegation,Current Community Delegation,New Base Delegation,Self Delegation Bonus,Community Delegation Bonus,Total Delegation,Current Voting Power,Updated Voting Power,Delta Voting Power\n'
+  let buffer = 'Moniker,OperatorAddress,Commission,Number of Delegations,Current Self Delegation,Current Community Delegation,New Base Delegation,Self Delegation Bonus,Community Delegation Bonus,Total Delegation,Current Voting Power,Updated Voting Power,Delta Voting Power\n'
   for (const target of targets) {
     const currentVotingPower = target.currentDelegations.totalDelegation / currentTotalDelegations
     const updatedVotingPower = (target.totalDelegation + target.currentDelegations.selfDelegation + target.currentDelegations.communityDelegation) / updatedTotalDelegations
     const deltaVotingPower = updatedVotingPower - currentVotingPower
-    buffer += `${target.moniker},${target.commission.toFixed(2)},` +
+    buffer += `${target.moniker},${target.operatorAddress},${target.commission.toFixed(2)},` +
     `${target.currentDelegations.numDelegators},${(target.currentDelegations.selfDelegation / ungm).toFixed(0)},${(target.currentDelegations.communityDelegation / ungm).toFixed(0)},` +
     `${(target.baseDelegation / ungm).toFixed(0)},${(target.selfDelegationBonus / ungm).toFixed(0)},${(target.communityDelegationBonus / ungm).toFixed(0)},${(target.totalDelegation / ungm).toFixed(0)},` +
     `${currentVotingPower.toFixed(4)},${updatedVotingPower.toFixed(4)},${deltaVotingPower.toFixed(4)}\n`
@@ -154,3 +237,5 @@ function writeCsv (targets: Target[], fileName: string) {
 const validators = await client.staking.validators()
 const targets = await createTargets(validators)
 writeCsv(targets, 'allocations.csv')
+const messages = await createMessages(targets)
+writeTransaction(messages, 'treasury-delegations.json')
